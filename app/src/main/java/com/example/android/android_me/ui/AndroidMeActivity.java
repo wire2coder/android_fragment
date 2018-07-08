c9f18a8e72e48648577431750898f17df2da7c1b
Andy Murph <murph@email.com>
2018-07-07 23:28:07 -0400

    work: created head fragment

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,6 +16,8 @@
 
 package com.example.android.android_me.ui;
 
+
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -28,5 +30,18 @@ public class AndroidMeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
-    }
-}
+
+
+        BodyPartFragment headFragment = new BodyPartFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add( R.id.head_container, headFragment )
+                .commit();
+
+    } // onCreate()
+
+
+
+} // class AndroidMeActivity
